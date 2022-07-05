7dd941781aee0c8b3eb342122b3fde09913332d6
João Antônio Misson Milhorim <joao.misson@gmail.com>
2022-07-05 06:04:33 -0300

    atualizei o html e css do perfil

--- milestone2/shop/src/views/PerfilView.vue
+++ milestone2/shop/src/views/PerfilView.vue
@@ -1,57 +1,73 @@
 <template>
-    <div class="content_perfil">
-                <div class="titulo_perfil">
-                    <h1>Perfil do Usuário</h1>  
-                    <h2>Home > Meu Perfil</h2>
-                    <hr/>
-                </div>
-
-
-                <div id="info">
-
-                    <img src="@/assets/image/icons/prof-pic.png" class="foto">
-
-                    <div id="campos">
-
-                        <div id="esq">
-                            <div id="div_nome" class="form">
-                                <h3> Nome </h3>
-                                <div class="dados"><span></span></div>
-                            </div>
-
-                            <div id="div_cpf" class="form">
-                                <h3> CPF </h3>
-                                <div class="dados"><span></span></div>
-                            </div>
-                            
-                            <div id="div_tel" class="form">
-                                <h3> Telefone </h3>
-                                <div class="dados"><span></span></div>
-                            </div>
-
-                        </div>
-                        <div id="dir">
-                            <div id="div_sobre" class="form">
-                                <h3> Sobrenome </h3>
-                                <div class="dados"><span></span></div>
-                            </div>
-
-                            <div id="div_email" class="form">
-                                <h3> Email </h3>
-                                <div class="dados"><span></span></div>
-                            </div>
-
-                            <div id="div_end" class="form">
-                                <h3> Endereço </h3>
-                                <div class="dados"><span></span></div>
-                            </div>
-                        </div>
+    <div class="content-perfil">
+        <div class="sub-header">
+            <h1 class="profile-title"><b>Perfil do Usuário</b></h1>
+            <h2 class="profile-path"><b>Home > Meu Perfil</b></h2>
+        </div>
+        <div class="line top-line">
+            <hr/>
+        </div>
+        <div class="personal-info">
+            <div class="left-column">
+                <div class="field field-nome">
+                    <div class="label label-nome">
+                        <h3>Nome</h3>
                     </div>
-                    
-                    <button id="edit"> Editar perfil </button>
-                    <hr/>
+                    <div class="data data-nome">
+                        <input type="text" value="teste">
+                    </div>
+                </div>
+                <div class="field field-cpf">
+                    <div class="label label-cpf">
+                        <h3>CPF</h3>
+                    </div>
+                    <div class="data data-cpf">
+                        <input type="text" value="teste">
+                    </div>
+                </div>
+                <div class="field field-telefone">
+                    <div class="label label-telefone">
+                        <h3>Telefone</h3>
+                    </div>
+                    <div class="data data-telefone">
+                        <input type="text" value="teste">
+                    </div>
                 </div>
             </div>
+            <div class="right-column">
+                <div class="field field-sobrenome">
+                    <div class="label label-sobrenome">
+                        <h3>Sobrenome</h3>
+                    </div>
+                    <div class="data data-sobrenone">
+                        <input type="text" value="teste">
+                    </div>
+                </div>
+                <div class="field field-endereco">
+                    <div class="label label-endereco">
+                        <h3>Endereco</h3>
+                    </div>
+                    <div class="data data-endereco">
+                        <input type="text" value="teste">
+                    </div>
+                </div>
+                <div class="field field-email">
+                    <div class="label label-email">
+                        <h3>Email</h3>
+                    </div>
+                    <div class="data data-email">
+                        <input type="text" value="teste">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="saveChanges">
+            <button class="saveChanges-btn">Confirmar alterações</button>
+        </div>
+        <div class="line bottom-line">
+            <hr/>
+        </div>
+    </div>
 </template>
 
 <script>
