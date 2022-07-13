88c21e128d91859a2d9406616f94fa4626226373
MarcusHuzi <marcushuziwara@usp.br>
2022-07-13 17:01:06 -0300

    arrumando problema do schema

--- milestone3/server/src/app.js
+++ milestone3/server/src/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import Client from './models/clients.js';
 import index from './routes/index.js';
 import mongoose from 'mongoose';
-import Client from './models/clients.js';
+
 
 mongoose.connect('mongodb://localhost:27017/mercado')
 
