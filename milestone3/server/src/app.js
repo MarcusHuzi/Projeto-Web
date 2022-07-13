c867068fdbb1ac65a989c8a2f30c5731e61bb4e3
MarcusHuzi <marcushuziwara@usp.br>
2022-07-13 16:39:40 -0300

    realocando arquivos

--- milestone3/server/src/app.js
+++ milestone3/server/src/app.js
@@ -4,10 +4,11 @@ import index from './routes/index.js';
 import mongoose from 'mongoose';
 import Client from './models/clients.js';
 
+mongoose.connect('mongodb://localhost:27017/mercado')
+
 const app = express();
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extend: false}));
-mongoose.connect('mongodb://localhost:27017/mercado')
 
 app.use('/store', index);
 
