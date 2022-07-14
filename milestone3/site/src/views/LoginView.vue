c679f9c3311685d1466be8579884d3cbf0c7b5da
João Antônio Misson Milhorim <joao.misson@gmail.com>
2022-07-14 11:25:05 -0300

    fazendo o login

--- milestone3/site/src/views/LoginView.vue
+++ milestone3/site/src/views/LoginView.vue
@@ -6,16 +6,16 @@
 
             <div class="email-field">
                 <p id="email-label">Email</p>
-                <input class="email-input" type="email">
+                <input class="email-input" type="email" name="email" autocomplete="on" placeholder="Seu e-mail aqui" v-model="email">
             </div>
                     
             <div class="senha-field">
                 <p id="senha-label">Senha</p>
-                <input type="password" name="username">
+                <input type="password" name="senha" maxlength="26" v-model="password" placeholder="Sua senha aqui">
             </div>
 
             <div class="entrar-field">
-                <input id="entrar-btn" type="submit" value="Entrar">
+                <input id="entrar-btn" type="submit" value="Entrar" @click="login()">
             </div>
 
             <div class="create-account-field">
@@ -28,6 +28,44 @@
 <script>
     export default {
         name: 'LoginView',
+        data() {
+            return {
+                email: "",
+                password: ""
+            }
+        },
+
+        methods: {
+            login: async function() {
+                if(this.email != "" && this.password != "") {
+                    try {
+						const req_body = JSON.stringify({
+                            password: this.password,
+							email: this.email
+						});	
+
+						let resp = await fetch("http://localhost:3000/clients/ck_email"+this.email, { 
+                            method: 'POST', 
+                            body: req_body,
+                            headers: { 'Content-Type': 'application/json' }
+                        });
+
+                        if (resp.status === 201) {
+                            alert("usuario valido");
+                        }
+                        else {
+                            alert("usuario invalido");
+                        }
+                    }
+                    catch(e) {
+                        alert("Erro na verificação do login");
+                    }
+                }
+                else {
+                    alert("Preencha os campos de Email e Senha");
+                }
+            }
+        }
     }
 </script>
 
