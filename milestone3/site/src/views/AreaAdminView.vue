b1ccc1228fcd28a3a470a3b771eb68328bd1fe49
João Antônio Misson Milhorim <joao.misson@gmail.com>
2022-07-15 00:34:48 -0300

    pagina do AreaAdminView

--- milestone3/site/src/views/AreaAdminView.vue
+++ milestone3/site/src/views/AreaAdminView.vue
@@ -50,33 +50,33 @@
                 </div>
                 <div class="response-area users-response-area">
                     <div class="field">
-                        <label>Nome do Usuário</label><input class="nome" placeholder="Nome de usuário aqui">
+                        <label>Nome do Usuário</label><input required class="nome" placeholder="Nome de usuário aqui" type="text" v-model="nome">
                     </div>
                     <div class="field">
-                        <label>CPF</label><input class="cpf" placeholder="CPF aqui">
+                        <label>CPF</label><input required class="cpf" placeholder="CPF aqui" type="text" v-model="cpf">
                     </div>
                     <div class="field">
-                        <label>Email</label><input class="email" placeholder="Email aqui">
+                        <label>Email</label><input required class="email" placeholder="Email aqui" type="email" v-model="email">
                     </div>
                     <div class="field">
-                        <label>Telefone</label><input class="tel" placeholder="Telefone aqui">
+                        <label>Telefone</label><input required class="tel" placeholder="Telefone aqui" type="tel" v-model="celular">
                     </div>
                     <div class="field">
-                        <label>Data de nascimento</label><input class="nasc" placeholder="Data de nascimento aqui">
+                        <label>Data de nascimento</label><input required class="nasc" placeholder="Data de nascimento aqui" type="date" v-model="nasc">
                     </div>
                     <div class="field">
-                        <label>CEP</label><input class="cep" placeholder="CEP aqui">
+                        <label>CEP</label><input class="cep" required placeholder="CEP aqui" type="text" v-model="cep">
                     </div>
                     <div class="field">
-                        <label>Endereço</label><input class="endereco" placeholder="Endereço aqui">
+                        <label>Endereço</label><input required class="endereco" placeholder="Endereço aqui" type="text" v-model="end">
                     </div>
                     <div class="field adm-checkbox">
                         <label class="isAdm-lavel">É administrador:</label>
-                        <input class="isAdm" type="checkbox">
+                        <input required class="isAdm" type="checkbox" v-model="isAdm">
                     </div>
                 </div>
                 <div class="save-changes users-save-changes">
-                    <button class="save-changes-btn users-save-changes-btn">Salvar alterações</button>
+                    <button class="save-changes-btn users-save-changes-btn" @click="verificar()">Salvar alterações</button>
                 </div>
             </div>
         </div>
@@ -84,30 +84,124 @@
 </template>
 
 <script>
-    import productsData from '../data/estoque.json'
-    import clientsData from '../data/clients.json'
-
+	let ano = new Date().getFullYear()
     export default {
         name: 'AreaAdminView',
         data() {
             return {
-                estoque: productsData,
-                clientes: clientsData,
-                mostrarCampos: false
+				email:"",
+				cpf:"",
+				cep:"",
+				end:"",
+				celular:"",
+				nome:"",
+				nasc:"",
+                isAdm: false,
+				erro: [0,0,0,0,0,0,0,0,0,0] //vetor para marcar erros
             };
         },
         methods: {
-            getImgUrl(url){
-                try {
-                    let str = require('@/assets/' + url)
-                    return str
-                }
-                catch {
-                    return ""
-                }
-            },
-            query() {
-                this.mostrarCampos = true;
+            verificar: async function() {
+                //verificar se o email esta no formato adequado
+				if(this.email == "" ){
+					document.getElementById("e_email").innerText = "Email inválido"
+					this.erro[2] = 1;
+				} else {
+					document.getElementById("e_email").innerText = "";
+					this.erro[2] = 0;
+				}
+
+				//verificar se o cpf ta no formato adequado
+				if(this.cpf==""){
+					document.getElementById("v_cpf").innerText = "CPF inválido"
+					this.erro[3] = 1;
+				} else {
+					document.getElementById("v_cpf").innerText = "";
+					this.erro[3] = 0;
+				}
+
+                //verificar se o cep ta no formato adequado
+				if(this.cep==""){
+					document.getElementById("v_cep").innerText = "CEP inválido"
+					this.erro[4] = 1;
+				} else{
+					document.getElementById("v_cep").innerText = "";
+					this.erro[4] = 0;
+				}
+
+                //verificar se nome foi inserido corretamente
+				if(this.nome == ""){
+					document.getElementById("v_nome").innerText = "Nome inválida"
+					this.erro[6] = 1;
+				} else{
+					document.getElementById("v_nome").innerText = "";
+					this.erro[6] = 0;
+				}
+
+                //verificar se celular foi inserido corretamente
+				if(this.celular == ""){
+					document.getElementById("v_celular").innerText = "Celular inválido"
+					this.erro[7] = 1;
+				} else{
+					document.getElementById("v_celular").innerText = "";
+					this.erro[7] = 0;
+				}
+
+                //verificar se o end foi inserido corretamente
+				if(this.end == ""){
+					document.getElementById("v_end").innerText = "Endereço inválido"
+					this.erro[8] = 1;
+				} else{
+					document.getElementById("v_end").innerText = "";
+					this.erro[8] = 0;
+				}
+
+                //verificar se a data de nascimento foi inserida corretamente
+				if(this.nasc == ""){
+					document.getElementById("v_nasc").innerText = "Data inválida"
+					this.erro[9] = 1;
+
+				} else if(ano - parseInt(this.nasc.substring(5,0)) < 14){
+					document.getElementById("v_nasc").innerText = "Data inválida"
+					this.erro[9] = 1;
+		
+				}else{
+					document.getElementById("v_nasc").innerText = "";
+					this.erro[9] = 0;
+				}
+
+                if(this.erro[0]==1 || this.erro[1]==1 || this.erro[2]==1 || this.erro[3]==1 || this.erro[4]==1 || this.erro[5]==1
+					|| this.erro[6]==1 || this.erro[7]==1 || this.erro[8]==1 || this.erro[9]==1) {
+					alert("Informações de usuário inválidas");
+				} else {
+					try{
+						let req = JSON.stringify({
+							nome: this.nome,
+							cpf: this.cpf,
+							email: this.email,
+							tel: this.celular,
+							nasc: this.nasc,
+							senha: this.password,
+							cep: this.cep,
+							endereco: this.end,
+						});				
+						let resp = await fetch("http://localhost:3000/clients/cadastro", {
+							method: 'POST',
+							headers: { 'Content-Type': 'application/json' },
+							body: req
+						});
+						let resp_json = await resp.json();
+						alert(resp_json.message);
+
+						if(resp.status == 200){
+							this.$cookies.set("account_id", resp_json.id);
+							this.$router.push('/areaAdmin')
+						}
+					}
+                    catch(e){
+						alert('Error:' + e);
+					}
+				}
             }
         }
     };
