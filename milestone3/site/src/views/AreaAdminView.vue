a5f523faecfa84f6f707c24681e73f4f477b2525
João Antônio Misson Milhorim <joao.misson@gmail.com>
2022-07-14 22:22:09 -0300

    pagina de admin mais ou menos feita

--- milestone3/site/src/views/AreaAdminView.vue
+++ milestone3/site/src/views/AreaAdminView.vue
@@ -19,16 +19,26 @@
                         </div>
                         <div class="query-input-area products-query-input-area">
                             <input class="query-input products-query-input" placeholder="Slug do produto aqui">
-                            <button class="query-btn products-query-btn">Consultar</button>
+                            <button class="query-btn products-query-btn" @click="query()">Consultar</button>
                         </div>
                     </div>
                     <div class="response-area users-response-area">
                         <img class="product-img">
-                        <input class="title">
-                        <input class="description">
-                        <input class="price">
-                        <input class="category">
-                        <input class="in_stock">
+                        <label>Nome do produto</label><input class="title">
+                        <label>Descrição</label><input class="description" >
+                        <label>Preço</label><input class="price">
+                        <label>Categoria</label>
+                        <select class="category">
+                            <option value="bebidas">Bebidas</option>
+                            <option value="hortifruti" selected>Hortifruti</option>
+                            <option value="laticinios">Laticinios</option>
+                            <option value="congelados">Congelados</option>
+                            <option value="enlatados">Enlatados</option>
+                            <option value="cereais">Cereais</option>
+                            <option value="higiene">Higiene</option>
+                            <option value="limpeza">Limpeza</option>
+                        </select>
+                        <label>Quantidade no estoque</label><input class="in_stock">
                     </div>
                     <div class="save-changes users-save-changes">
                         <button class="save-changes-btn users-save-changes-btn">Salvar alterações</button>
@@ -51,14 +61,14 @@
                         </div>
                     </div>
                     <div class="response-area users-response-area">
-                        <input class="nome">
-                        <input class="cpf">
-                        <input class="email">
-                        <input class="tel">
-                        <input class="nasc">
-                        <input class="cep">
-                        <input class="endereco">
-                        <input class="isAdm">
+                        <label>Nome</label><input class="nome">
+                        <label>CPF</label><input class="cpf">
+                        <label>Email</label><input class="email">
+                        <label>Telefone</label><input class="tel">
+                        <label>Data de nascimento</label><input class="nasc">
+                        <label>CEP</label><input class="cep">
+                        <label>Endereço</label><input class="endereco">
+                        <label>É administrador</label><input class="isAdm" type="checkbox">
                     </div>
                     <div class="save-changes users-save-changes">
                         <button class="save-changes-btn users-save-changes-btn">Salvar alterações</button>
@@ -90,6 +100,9 @@
                 catch {
                     return ""
                 }
+            },
+            query() {
+                alert("PORRA")
             }
         }
     };
