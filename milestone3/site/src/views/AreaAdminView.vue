7ac2a66da642bea2efc45f416627b081ea49ae12
João Antônio Misson Milhorim <joao.misson@gmail.com>
2022-07-15 00:47:57 -0300

    admin denovo

--- milestone3/site/src/views/AreaAdminView.vue
+++ milestone3/site/src/views/AreaAdminView.vue
@@ -84,7 +84,6 @@
 </template>
 
 <script>
-	let ano = new Date().getFullYear()
     export default {
         name: 'AreaAdminView',
         data() {
@@ -97,111 +96,44 @@
 				nome:"",
 				nasc:"",
                 isAdm: false,
-				erro: [0,0,0,0,0,0,0,0,0,0] //vetor para marcar erros
             };
         },
         methods: {
             verificar: async function() {
-                //verificar se o email esta no formato adequado
-				if(this.email == "" ){
-					document.getElementById("e_email").innerText = "Email inválido"
-					this.erro[2] = 1;
-				} else {
-					document.getElementById("e_email").innerText = "";
-					this.erro[2] = 0;
-				}
+                if(this.email ==""){
+                    alert("Preencha o campo Email");
+                    return;
+                }
 
-				//verificar se o cpf ta no formato adequado
-				if(this.cpf==""){
-					document.getElementById("v_cpf").innerText = "CPF inválido"
-					this.erro[3] = 1;
-				} else {
-					document.getElementById("v_cpf").innerText = "";
-					this.erro[3] = 0;
-				}
+                if(this.cpf == ""){
+                   alert("Preencha o campo CPF");
+                   return;
+                }
 
-                //verificar se o cep ta no formato adequado
-				if(this.cep==""){
-					document.getElementById("v_cep").innerText = "CEP inválido"
-					this.erro[4] = 1;
-				} else{
-					document.getElementById("v_cep").innerText = "";
-					this.erro[4] = 0;
-				}
+                if(this.cep == ""){
+                   alert("Preencha o campo CEP");
+                   return;
+                }
 
-                //verificar se nome foi inserido corretamente
-				if(this.nome == ""){
-					document.getElementById("v_nome").innerText = "Nome inválida"
-					this.erro[6] = 1;
-				} else{
-					document.getElementById("v_nome").innerText = "";
-					this.erro[6] = 0;
-				}
+                if(this.end == ""){
+                   alert("Preencha o campo Endereço");
+                   return;
+                }
 
-                //verificar se celular foi inserido corretamente
-				if(this.celular == ""){
-					document.getElementById("v_celular").innerText = "Celular inválido"
-					this.erro[7] = 1;
-				} else{
-					document.getElementById("v_celular").innerText = "";
-					this.erro[7] = 0;
-				}
+                if(this.celular == ""){
+                   alert("Preencha o campo Celular");
+                   return;
+                }
 
-                //verificar se o end foi inserido corretamente
-				if(this.end == ""){
-					document.getElementById("v_end").innerText = "Endereço inválido"
-					this.erro[8] = 1;
-				} else{
-					document.getElementById("v_end").innerText = "";
-					this.erro[8] = 0;
-				}
+                if(this.nome == ""){
+                   alert("Preencha o campo Nome");
+                   return;
+                }
 
-                //verificar se a data de nascimento foi inserida corretamente
-				if(this.nasc == ""){
-					document.getElementById("v_nasc").innerText = "Data inválida"
-					this.erro[9] = 1;
-
-				} else if(ano - parseInt(this.nasc.substring(5,0)) < 14){
-					document.getElementById("v_nasc").innerText = "Data inválida"
-					this.erro[9] = 1;
-		
-				}else{
-					document.getElementById("v_nasc").innerText = "";
-					this.erro[9] = 0;
-				}
-
-                if(this.erro[0]==1 || this.erro[1]==1 || this.erro[2]==1 || this.erro[3]==1 || this.erro[4]==1 || this.erro[5]==1
-					|| this.erro[6]==1 || this.erro[7]==1 || this.erro[8]==1 || this.erro[9]==1) {
-					alert("Informações de usuário inválidas");
-				} else {
-					try{
-						let req = JSON.stringify({
-							nome: this.nome,
-							cpf: this.cpf,
-							email: this.email,
-							tel: this.celular,
-							nasc: this.nasc,
-							senha: this.password,
-							cep: this.cep,
-							endereco: this.end,
-						});				
-						let resp = await fetch("http://localhost:3000/clients/cadastro", {
-							method: 'POST',
-							headers: { 'Content-Type': 'application/json' },
-							body: req
-						});
-						let resp_json = await resp.json();
-						alert(resp_json.message);
-
-						if(resp.status == 200){
-							this.$cookies.set("account_id", resp_json.id);
-							this.$router.push('/areaAdmin')
-						}
-					}
-                    catch(e){
-						alert('Error:' + e);
-					}
-				}
+                if(this.nasc == ""){
+                   alert("Preencha o campo Data de Nascimento");
+                   return;
+                }
             }
         }
     };
